9698554982ca3283c82d5e44b4859bef8439d0b5
Nguyen Ngoc Bang <bang270197@gmail.com>
2021-07-28 09:51:22 +0700

    build 1

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const URL = "http://localhost:8081";
+const URL = "103.109.43.20:5002";
 
+// const URL = "http://localhost:5000";
 export const fetchPosts = () => axios.get(`${URL}/posts`);
 export const createPost = (payload) => axios.post(`${URL}/posts`, payload);
 export const updatePost = (payload) =>
